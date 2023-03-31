757e924d67479daa502a216ac0bb14b3fdf4dc44
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2023-03-31 07:13:57 +0500

    Included express-async-errors

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-require('dotenv').config();
+require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
-const bodyParser = require("body-parser")
-const connectDB = require("./db/connect")
-const productsRouter = require('./routes/products')
+const bodyParser = require("body-parser");
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 // middleware
-const errorHandler = require('./middleware/errorHandler')
-const notFound = require('./middleware/notFound')
+const errorHandler = require("./middleware/errorHandler");
+const notFound = require("./middleware/notFound");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //app.use()
 
-
 // routes
-app.get('/',(req, res)=>{
-  res.send('Store API')
-})
+app.get("/", (req, res) => {
+  res.send("Store API");
+});
 
 // product routes
 
-app.use('/api/v1/products',productsRouter)
+app.use("/api/v1/products", productsRouter);
 
-
-app.use(errorHandler)
-app.use(notFound)
-
+app.use(errorHandler);
+app.use(notFound);
 
 const start = async () => {
   try {
@@ -40,6 +38,6 @@ const start = async () => {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-start()
+start();
