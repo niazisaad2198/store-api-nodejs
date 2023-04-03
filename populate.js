16cc3108757231d0daadc01ba413f45345bee977
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2023-04-03 20:04:31 +0500

    Fix: Script now ends after completing.

--- populate.js
+++ populate.js
@@ -11,8 +11,10 @@ const start = async () => {
     await Product.deleteMany(); // clear the entire collection
     await Product.create(jsonProducts); // populate the collection
     console.log("Success!!!");
+    process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
